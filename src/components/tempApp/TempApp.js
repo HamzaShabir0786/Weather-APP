a2d36204e5b45d971daa9a2c642571c379449673
Hamza Shabir <hamzii2205@gmail.com>
2024-11-25 01:56:25 +0500

    Project complete sucessfully and ready for deploy

--- src/components/tempApp/TempApp.js
+++ src/components/tempApp/TempApp.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 const TempApp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("karachi");
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=f7ea5c9eeb557102fa954c3b547d5230`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      setCity(resJson.main);
+    };
+    fetchAPI();
+  }, [search]);
   return (
     <>
       <div className="box">
@@ -6,18 +19,31 @@ const TempApp = () => {
         <div className="inputData">
           <input
             type="search"
-            placeholder="Enter city name"
+            placeholder="Enter City Name"
             className="inputFeild"
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
           ></input>
         </div>
         <>
-          <div className="info">
-            <h2 className="location">
-              <i className="fa-solid fa-street-view">Lahore</i>
-            </h2>
-            <h3 className="temp"></h3>
-            <h4 className="tempmin_max">Min : 5.5°Cel | Max : 5.25° Cel</h4>
-          </div>
+          {!city ? (
+            <p className="errorMessage">{search} Not data found</p>
+          ) : (
+            <div className="info">
+              <h2 className="location">
+                <i className="fa-solid fa-street-view">{search} </i>
+              </h2>
+              <h3 className="temp">{city.temp}°Cel</h3>
+              <h4 className="tempmin_max">
+                Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel
+              </h4>
+              <p className="copyRight">
+                &copy; 2024 Hamza Shabir. All rights reserved.
+              </p>
+            </div>
+          )}
         </>
       </div>
     </>
